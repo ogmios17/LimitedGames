07f93364a62c58de461e8ae3a6c18267fcda3929
AndreaFerreri-hub <andreaferreri04@gmail.com>
2025-04-02 22:45:11 +0200

    Update Catalogo.sql
    Aggiunte le edizioni

---
+++ LimitedGames/src/main/db/Catalogo.sql
@@ -0,0 +1,46 @@
+DROP DATABASE IF EXISTS Catalogo;
+CREATE DATABASE Catalogo;
+USE LimitedGames;
+
+INSERT INTO Gioco (ID, Prezzo, IVA, Data_uscita, Descrizione, Titolo, immagine, Edizione, Sconto) values
+('7821-5649', '69,99', '12.62', '25\02\2022', 'Elden Ring è un videogioco action RPG open world facente parte della saga dei suols like.', 'ELDEN RING', 'IMMAGINE', 'Standard', '0,00');
+INSERT INTO Gioco (ID, Prezzo, IVA, Data_uscita, Descrizione, Titolo, immagine, Edizione, Sconto) values
+('7821-5649', '89,99', '16.23', '25\02\2022', 'Elden Ring è un videogioco action RPG open world facente parte della saga dei suols like.', 'ELDEN RING', 'IMMAGINE', 'Delux Edition', '0,00');
+INSERT INTO Gioco (ID, Prezzo, IVA, Data_uscita, Descrizione, Titolo, immagine, Edizione, Sconto) values
+('7821-5649', '109,99', '19,83', '25\02\2022', 'Elden Ring è un videogioco action RPG open world facente parte della saga dei suols like.', 'ELDEN RING', 'IMMAGINE', 'GOTY Edition', '0,00');
+INSERT INTO Gioco (ID, Prezzo, IVA, Data_uscita, Descrizione, Titolo, immagine, Edizione, Sconto) values
+('1478-5118', '59,99', '10.82', '09\11\2022', 'Un epico action adventure che unisce mitologia e dramma familiare.', 'God of War Ragnarök', '', 'Standard', '0,00');
+INSERT INTO Gioco (ID, Prezzo, IVA, Data_uscita, Descrizione, Titolo, immagine, Edizione, Sconto) values
+('1478-5118', '79,99', '14.42', '09\11\2022', 'Un epico action adventure che unisce mitologia e dramma familiare.', 'God of War Ragnarök', '', 'Delux', '0,00');
+INSERT INTO Gioco (ID, Prezzo, IVA, Data_uscita, Descrizione, Titolo, immagine, Edizione, Sconto) values
+('2345-6789', '69,99', '12.62', '18\02\2022', 'Avventura post-apocalittica in un vasto open world ricco di misteri.', 'Horizon Forbidden West', '', 'Standard', '15,00');
+INSERT INTO Gioco (ID, Prezzo, IVA, Data_uscita, Descrizione, Titolo, immagine, Edizione, Sconto) values
+('2345-6789', '99,99', '18.03', '18\02\2022', 'Avventura post-apocalittica in un vasto open world ricco di misteri.', 'Horizon Forbidden West', '', 'Complete edition', '25,00');
+INSERT INTO Gioco (ID, Prezzo, IVA, Data_uscita, Descrizione, Titolo, immagine, Edizione, Sconto) values
+('3456-7890', '49,99', '9.01', '12\11\2020', 'Un dinamico action-adventure con movimenti acrobatici e combattimenti fluidi.', 'Spider-Man: Miles Morales', '', 'Standard', '20,00');
+INSERT INTO Gioco (ID, Prezzo, IVA, Data_uscita, Descrizione, Titolo, immagine, Edizione, Sconto) values
+('3456-7890', '59,99', '10.82', '12\11\2020', 'Un dinamico action-adventure con movimenti acrobatici e combattimenti fluidi.', 'Spider-Man: Miles Morales', '', 'Ultimate edition', '10,00');
+INSERT INTO Gioco (ID, Prezzo, IVA, Data_uscita, Descrizione, Titolo, immagine, Edizione, Sconto) values
+('4567-8901', '69,99', '12.62', '12\11\2020', 'Remake mozzafiato di un classico, con grafica e gameplay rinnovati.', 'Demon''s Souls', '', 'Standard', '15,00');
+INSERT INTO Gioco (ID, Prezzo, IVA, Data_uscita, Descrizione, Titolo, immagine, Edizione, Sconto) values
+('4567-8901', '89,99', '16.23', '12\11\2020', 'Remake mozzafiato di un classico, con grafica e gameplay rinnovati.', 'Demon''s Souls', '', 'Delux edition', '10,00');
+INSERT INTO Gioco (ID, Prezzo, IVA, Data_uscita, Descrizione, Titolo, immagine, Edizione, Sconto) values
+('5678-9012', '59,99', '10.82', '11\06\2021', 'Un platform d’azione che sfrutta al massimo le potenzialità del PS5.', 'Ratchet & Clank: Rift Apart', '', 'Standard', '10,00');
+INSERT INTO Gioco (ID, Prezzo, IVA, Data_uscita, Descrizione, Titolo, immagine, Edizione, Sconto) values
+('5678-9012', '69,99', '12.62', '11\06\2021', 'Un platform d’azione che sfrutta al massimo le potenzialità del PS5.', 'Ratchet & Clank: Rift Apart', '', 'Delux edition', '0,00');
+INSERT INTO Gioco (ID, Prezzo, IVA, Data_uscita, Descrizione, Titolo, immagine, Edizione, Sconto) values
+('6789-0123', '39,99', '7.21', '19\06\2020', 'Un intenso survival drama con narrazione emozionante e ambientazioni realistiche.', 'The Last of Us Part II', '', 'Standard', '0,00');
+INSERT INTO Gioco (ID, Prezzo, IVA, Data_uscita, Descrizione, Titolo, immagine, Edizione, Sconto) values
+('6789-0123', '49,99', '9.01', '19\06\2020', 'Un intenso survival drama con narrazione emozionante e ambientazioni realistiche.', 'The Last of Us Part II', '', 'Remastered', '0,00');
+ INSERT INTO Gioco (ID, Prezzo, IVA, Data_uscita, Descrizione, Titolo, immagine, Edizione, Sconto) values
+('7890-1234', '29,99', '5.41', '10\05\2016', 'Un’avventura d’azione ricca di suspense e colpi di scena, con ambientazioni esotiche.', 'Uncharted 4: A Thief''s End', '', 'Standard', '10,00');
+INSERT INTO Gioco (ID, Prezzo, IVA, Data_uscita, Descrizione, Titolo, immagine, Edizione, Sconto) values
+('7890-1234', '79,99', '14.42', '10\05\2016', 'Un’avventura d’azione ricca di suspense e colpi di scena, con ambientazioni esotiche.', 'Uncharted 4: A Thief''s End', '', 'Special edition', '20,00');
+INSERT INTO Gioco (ID, Prezzo, IVA, Data_uscita, Descrizione, Titolo, immagine, Edizione, Sconto) values
+('8901-2345', '49,99', '9.01', '17\07\2020', 'Un open world che miscela stealth e combattimenti epici in un Giappone feudale.', 'Ghost of Tsushima', '', 'Standard', '10,00');
+INSERT INTO Gioco (ID, Prezzo, IVA, Data_uscita, Descrizione, Titolo, immagine, Edizione, Sconto) values
+('8901-2345', '69,99', '12.62', '17\07\2020', 'Un open world che miscela stealth e combattimenti epici in un Giappone feudale.', 'Ghost of Tsushima', '', 'Definitive Edition', '10,00');
+INSERT INTO Gioco (ID, Prezzo, IVA, Data_uscita, Descrizione, Titolo, immagine, Edizione, Sconto) values
+('9012-3456', '79,99', '14.42', '07\05\2021', 'Un survival horror con atmosfere claustrofobiche e un ritmo di gioco incalzante.', 'Resident Evil Village', '', 'Standard', '25,00');
+INSERT INTO Gioco (ID, Prezzo, IVA, Data_uscita, Descrizione, Titolo, immagine, Edizione, Sconto) values
+('9012-3456', '99,99', '18.03', '07\05\2021', 'Un survival horror con atmosfere claustrofobiche e un ritmo di gioco incalzante.', 'Resident Evil Village', '', 'Gold Edition', '30,00');
