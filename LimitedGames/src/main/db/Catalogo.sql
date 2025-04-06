a0eb984577a78504cebbe9086671efbf8818e709
oghamSA <o.dellerba@studenti.unisa.it>
2025-04-06 13:29:00 +0200

    Update Catalogo.sql
    Made fixes to data format

--- LimitedGames/src/main/db/Catalogo.sql
+++ LimitedGames/src/main/db/Catalogo.sql
@@ -1,46 +1,65 @@
-DROP DATABASE IF EXISTS Catalogo;
-CREATE DATABASE Catalogo;
 USE LimitedGames;
 
-INSERT INTO Gioco (ID, Prezzo, IVA, Data_uscita, Descrizione, Titolo, immagine, Edizione, Sconto) values
-('7821-5649', '69,99', '12.62', '25\02\2022', 'Elden Ring è un videogioco action RPG open world facente parte della saga dei suols like.', 'ELDEN RING', 'C:\Users\andre\OneDrive\Desktop\Roba di WEB\Immagini-db\elden ring.jpeg', 'Standard', '0,00');
-INSERT INTO Gioco (ID, Prezzo, IVA, Data_uscita, Descrizione, Titolo, immagine, Edizione, Sconto) values
-('7821-5649', '89,99', '16.23', '25\02\2022', 'Elden Ring è un videogioco action RPG open world facente parte della saga dei suols like.', 'ELDEN RING', 'C:\Users\andre\OneDrive\Desktop\Roba di WEB\Immagini-db\elden ring.jpeg', 'Delux Edition', '0,00');
-INSERT INTO Gioco (ID, Prezzo, IVA, Data_uscita, Descrizione, Titolo, immagine, Edizione, Sconto) values
-('7821-5649', '109,99', '19,83', '25\02\2022', 'Elden Ring è un videogioco action RPG open world facente parte della saga dei suols like.', 'ELDEN RING', 'C:\Users\andre\OneDrive\Desktop\Roba di WEB\Immagini-db\elden ring.jpeg', 'GOTY Edition', '0,00');
-INSERT INTO Gioco (ID, Prezzo, IVA, Data_uscita, Descrizione, Titolo, immagine, Edizione, Sconto) values
-('1478-5118', '59,99', '10.82', '09\11\2022', 'Un epico action adventure che unisce mitologia e dramma familiare.', 'God of War Ragnarök', 'C:\Users\andre\OneDrive\Desktop\Roba di WEB\Immagini-db\God-of-War - Copia.jpg', 'Standard', '0,00');
-INSERT INTO Gioco (ID, Prezzo, IVA, Data_uscita, Descrizione, Titolo, immagine, Edizione, Sconto) values
-('1478-5118', '79,99', '14.42', '09\11\2022', 'Un epico action adventure che unisce mitologia e dramma familiare.', 'God of War Ragnarök', 'C:\Users\andre\OneDrive\Desktop\Roba di WEB\Immagini-db\God-of-War - Copia.jpg', 'Delux', '0,00');
-INSERT INTO Gioco (ID, Prezzo, IVA, Data_uscita, Descrizione, Titolo, immagine, Edizione, Sconto) values
-('2345-6789', '69,99', '12.62', '18\02\2022', 'Avventura post-apocalittica in un vasto open world ricco di misteri.', 'Horizon Forbidden West', 'C:\Users\andre\OneDrive\Desktop\Roba di WEB\Immagini-db\Horizon-Forbidden-West.avif', 'Standard', '15,00');
-INSERT INTO Gioco (ID, Prezzo, IVA, Data_uscita, Descrizione, Titolo, immagine, Edizione, Sconto) values
-('2345-6789', '99,99', '18.03', '18\02\2022', 'Avventura post-apocalittica in un vasto open world ricco di misteri.', 'Horizon Forbidden West', 'C:\Users\andre\OneDrive\Desktop\Roba di WEB\Immagini-db\Horizon-Forbidden-West.avif', 'Complete edition', '25,00');
-INSERT INTO Gioco (ID, Prezzo, IVA, Data_uscita, Descrizione, Titolo, immagine, Edizione, Sconto) values
-('3456-7890', '49,99', '9.01', '12\11\2020', 'Un dinamico action-adventure con movimenti acrobatici e combattimenti fluidi.', 'Spider-Man: Miles Morales', 'C:\Users\andre\OneDrive\Desktop\Roba di WEB\Immagini-db\SpiderMan.jpeg', 'Standard', '20,00');
-INSERT INTO Gioco (ID, Prezzo, IVA, Data_uscita, Descrizione, Titolo, immagine, Edizione, Sconto) values
-('3456-7890', '59,99', '10.82', '12\11\2020', 'Un dinamico action-adventure con movimenti acrobatici e combattimenti fluidi.', 'Spider-Man: Miles Morales', 'C:\Users\andre\OneDrive\Desktop\Roba di WEB\Immagini-db\SpiderMan.jpeg', 'Ultimate edition', '10,00');
-INSERT INTO Gioco (ID, Prezzo, IVA, Data_uscita, Descrizione, Titolo, immagine, Edizione, Sconto) values
-('4567-8901', '69,99', '12.62', '12\11\2020', 'Remake mozzafiato di un classico, con grafica e gameplay rinnovati.', 'Demon''s Souls', 'C:\Users\andre\OneDrive\Desktop\Roba di WEB\Immagini-db\Demon's-Souls', 'Standard', '15,00');
-INSERT INTO Gioco (ID, Prezzo, IVA, Data_uscita, Descrizione, Titolo, immagine, Edizione, Sconto) values
-('4567-8901', '89,99', '16.23', '12\11\2020', 'Remake mozzafiato di un classico, con grafica e gameplay rinnovati.', 'Demon''s Souls', 'C:\Users\andre\OneDrive\Desktop\Roba di WEB\Immagini-db\Demon's-Souls', 'Delux edition', '10,00');
-INSERT INTO Gioco (ID, Prezzo, IVA, Data_uscita, Descrizione, Titolo, immagine, Edizione, Sconto) values
-('5678-9012', '59,99', '10.82', '11\06\2021', 'Un platform d’azione che sfrutta al massimo le potenzialità del PS5.', 'Ratchet & Clank: Rift Apart', 'C:\Users\andre\OneDrive\Desktop\Roba di WEB\Immagini-db\Ratchet-&-Clank.avif', 'Standard', '10,00');
-INSERT INTO Gioco (ID, Prezzo, IVA, Data_uscita, Descrizione, Titolo, immagine, Edizione, Sconto) values
-('5678-9012', '69,99', '12.62', '11\06\2021', 'Un platform d’azione che sfrutta al massimo le potenzialità del PS5.', 'Ratchet & Clank: Rift Apart', 'C:\Users\andre\OneDrive\Desktop\Roba di WEB\Immagini-db\Ratchet-&-Clank.avif', 'Delux edition', '0,00');
-INSERT INTO Gioco (ID, Prezzo, IVA, Data_uscita, Descrizione, Titolo, immagine, Edizione, Sconto) values
-('6789-0123', '39,99', '7.21', '19\06\2020', 'Un intenso survival drama con narrazione emozionante e ambientazioni realistiche.', 'The Last of Us Part II', 'C:\Users\andre\OneDrive\Desktop\Roba di WEB\Immagini-db\The-Last-Of-Us-2.avif', 'Standard', '0,00');
-INSERT INTO Gioco (ID, Prezzo, IVA, Data_uscita, Descrizione, Titolo, immagine, Edizione, Sconto) values
-('6789-0123', '49,99', '9.01', '19\06\2020', 'Un intenso survival drama con narrazione emozionante e ambientazioni realistiche.', 'The Last of Us Part II', 'C:\Users\andre\OneDrive\Desktop\Roba di WEB\Immagini-db\The-Last-Of-Us-2.avif', 'Remastered', '0,00');
- INSERT INTO Gioco (ID, Prezzo, IVA, Data_uscita, Descrizione, Titolo, immagine, Edizione, Sconto) values
-('7890-1234', '29,99', '5.41', '10\05\2016', 'Un’avventura d’azione ricca di suspense e colpi di scena, con ambientazioni esotiche.', 'Uncharted 4: A Thief''s End', 'C:\Users\andre\OneDrive\Desktop\Roba di WEB\Immagini-db\Uncharted-4', 'Standard', '10,00');
-INSERT INTO Gioco (ID, Prezzo, IVA, Data_uscita, Descrizione, Titolo, immagine, Edizione, Sconto) values
-('7890-1234', '79,99', '14.42', '10\05\2016', 'Un’avventura d’azione ricca di suspense e colpi di scena, con ambientazioni esotiche.', 'Uncharted 4: A Thief''s End', 'C:\Users\andre\OneDrive\Desktop\Roba di WEB\Immagini-db\Uncharted-4', 'Special edition', '20,00');
-INSERT INTO Gioco (ID, Prezzo, IVA, Data_uscita, Descrizione, Titolo, immagine, Edizione, Sconto) values
-('8901-2345', '49,99', '9.01', '17\07\2020', 'Un open world che miscela stealth e combattimenti epici in un Giappone feudale.', 'Ghost of Tsushima', 'C:\Users\andre\OneDrive\Desktop\Roba di WEB\Immagini-db\Ghost-Of-Tsushima.avif', 'Standard', '10,00');
-INSERT INTO Gioco (ID, Prezzo, IVA, Data_uscita, Descrizione, Titolo, immagine, Edizione, Sconto) values
-('8901-2345', '69,99', '12.62', '17\07\2020', 'Un open world che miscela stealth e combattimenti epici in un Giappone feudale.', 'Ghost of Tsushima', 'C:\Users\andre\OneDrive\Desktop\Roba di WEB\Immagini-db\Ghost-Of-Tsushima.avif', 'Definitive Edition', '10,00');
-INSERT INTO Gioco (ID, Prezzo, IVA, Data_uscita, Descrizione, Titolo, immagine, Edizione, Sconto) values
-('9012-3456', '79,99', '14.42', '07\05\2021', 'Un survival horror con atmosfere claustrofobiche e un ritmo di gioco incalzante.', 'Resident Evil Village', 'C:\Users\andre\OneDrive\Desktop\Roba di WEB\Immagini-db\Resident-Evil-Village.jpg', 'Standard', '25,00');
-INSERT INTO Gioco (ID, Prezzo, IVA, Data_uscita, Descrizione, Titolo, immagine, Edizione, Sconto) values
-('9012-3456', '99,99', '18.03', '07\05\2021', 'Un survival horror con atmosfere claustrofobiche e un ritmo di gioco incalzante.', 'Resident Evil Village', 'C:\Users\andre\OneDrive\Desktop\Roba di WEB\Immagini-db\Resident-Evil-Village.jpg', 'Gold Edition', '30,00');
+INSERT INTO Gioco (Prezzo, IVA, Data_uscita, Descrizione, Titolo, immagine, Edizione, Sconto) 
+VALUES('69.99', '12.62', '2022-02-25', 'Elden Ring è un videogioco action RPG open world facente parte della saga dei suols like.', 'ELDEN RING', 'elden ring.jpeg', 'Standard', '0.00');
+
+INSERT INTO Gioco (Prezzo, IVA, Data_uscita, Descrizione, Titolo, immagine, Edizione, Sconto) 
+VALUES('89.99', '16.23', '2022-02-25', 'Elden Ring è un videogioco action RPG open world facente parte della saga dei suols like.', 'ELDEN RING', 'elden ring.jpeg', 'Delux Edition', '0.00');
+
+INSERT INTO Gioco (Prezzo, IVA, Data_uscita, Descrizione, Titolo, immagine, Edizione, Sconto) 
+VALUES('109.99', '19.83', '2022-02-25', 'Elden Ring è un videogioco action RPG open world facente parte della saga dei suols like.', 'ELDEN RING', 'elden ring.jpeg', 'GOTY Edition', '0.00');
+
+INSERT INTO Gioco (Prezzo, IVA, Data_uscita, Descrizione, Titolo, immagine, Edizione, Sconto) 
+VALUES('59.99', '10.82', '2022-11-09', 'Un epico action adventure che unisce mitologia e dramma familiare.', 'God of War Ragnarök', 'God-of-War - Copia.jpg', 'Standard', '0.00');
+
+INSERT INTO Gioco (Prezzo, IVA, Data_uscita, Descrizione, Titolo, immagine, Edizione, Sconto) 
+VALUES('79.99', '14.42', '2022-11-09', 'Un epico action adventure che unisce mitologia e dramma familiare.', 'God of War Ragnarök', 'God-of-War - Copia.jpg', 'Delux', '0.00');
+
+INSERT INTO Gioco (Prezzo, IVA, Data_uscita, Descrizione, Titolo, immagine, Edizione, Sconto) 
+VALUES('69.99', '12.62', '2022-02-18', 'Avventura post-apocalittica in un vasto open world ricco di misteri.', 'Horizon Forbidden West', 'Horizon-Forbidden-West.avif', 'Standard', '15.00');
+
+INSERT INTO Gioco (Prezzo, IVA, Data_uscita, Descrizione, Titolo, immagine, Edizione, Sconto) 
+VALUES('99.99', '18.03', '2022-02-18', 'Avventura post-apocalittica in un vasto open world ricco di misteri.', 'Horizon Forbidden West', 'Horizon-Forbidden-West.avif', 'Complete edition', '25.00');
+
+INSERT INTO Gioco (Prezzo, IVA, Data_uscita, Descrizione, Titolo, immagine, Edizione, Sconto) 
+VALUES('49.99', '9.01', '2020-11-12', 'Un dinamico action-adventure con movimenti acrobatici e combattimenti fluidi.', 'Spider-Man: Miles Morales', 'SpiderMan.jpeg', 'Standard', '20.00');
+
+INSERT INTO Gioco (Prezzo, IVA, Data_uscita, Descrizione, Titolo, immagine, Edizione, Sconto) 
+VALUES('59.99', '10.82', '2020-11-12', 'Un dinamico action-adventure con movimenti acrobatici e combattimenti fluidi.', 'Spider-Man: Miles Morales', 'SpiderMan.jpeg', 'Ultimate edition', '10.00');
+
+INSERT INTO Gioco (Prezzo, IVA, Data_uscita, Descrizione, Titolo, immagine, Edizione, Sconto) 
+VALUES('69.99', '12.62', '2020-11-12', 'Remake mozzafiato di un classico, con grafica e gameplay rinnovati.', 'Demon''s Souls', 'Demons-Souls', 'Standard', '15.00');
+
+INSERT INTO Gioco (Prezzo, IVA, Data_uscita, Descrizione, Titolo, immagine, Edizione, Sconto) 
+VALUES('89.99', '16.23', '2020-11-12', 'Remake mozzafiato di un classico, con grafica e gameplay rinnovati.', 'Demon''s Souls', 'Demons-Souls', 'Deluxe edition', '10.00');
+
+INSERT INTO Gioco (Prezzo, IVA, Data_uscita, Descrizione, Titolo, immagine, Edizione, Sconto) 
+VALUES('59.99', '10.82', '2021-06-11', 'Un platform d’azione che sfrutta al massimo le potenzialità del PS5.', 'Ratchet & Clank: Rift Apart', 'Ratchet-&-Clank.avif', 'Standard', '10.00');
+
+INSERT INTO Gioco (Prezzo, IVA, Data_uscita, Descrizione, Titolo, immagine, Edizione, Sconto) 
+VALUES('69.99', '12.62', '2021-06-11', 'Un platform d’azione che sfrutta al massimo le potenzialità del PS5.', 'Ratchet & Clank: Rift Apart', 'Ratchet-&-Clank.avif', 'Delux edition', '0.00');
+
+INSERT INTO Gioco (Prezzo, IVA, Data_uscita, Descrizione, Titolo, immagine, Edizione, Sconto) 
+VALUES('39.99', '7.21', '2020-06-19', 'Un intenso survival drama con narrazione emozionante e ambientazioni realistiche.', 'The Last of Us Part II', 'The-Last-Of-Us-2.avif', 'Standard', '0.00');
+
+INSERT INTO Gioco (Prezzo, IVA, Data_uscita, Descrizione, Titolo, immagine, Edizione, Sconto) 
+VALUES('49.99', '9.01', '2020-06-19', 'Un intenso survival drama con narrazione emozionante e ambientazioni realistiche.', 'The Last of Us Part II', 'The-Last-Of-Us-2.avif', 'Remastered', '0.00');
+
+INSERT INTO Gioco (Prezzo, IVA, Data_uscita, Descrizione, Titolo, immagine, Edizione, Sconto) 
+VALUES('29.99', '5.41', '2016-05-10', 'Un’avventura d’azione ricca di suspense e colpi di scena, con ambientazioni esotiche.', 'Uncharted 4: A Thief''s End', 'Uncharted-4', 'Standard', '10.00');
+
+INSERT INTO Gioco (Prezzo, IVA, Data_uscita, Descrizione, Titolo, immagine, Edizione, Sconto) 
+VALUES('79.99', '14.42', '2016-05-10', 'Un’avventura d’azione ricca di suspense e colpi di scena, con ambientazioni esotiche.', 'Uncharted 4: A Thief''s End', 'Uncharted-4', 'Special edition', '20.00');
+
+INSERT INTO Gioco (Prezzo, IVA, Data_uscita, Descrizione, Titolo, immagine, Edizione, Sconto) 
+VALUES('49.99', '9.01', '2020-07-17', 'Un open world che miscela stealth e combattimenti epici in un Giappone feudale.', 'Ghost of Tsushima', 'Ghost-Of-Tsushima.avif', 'Standard', '10.00');
+
+INSERT INTO Gioco (Prezzo, IVA, Data_uscita, Descrizione, Titolo, immagine, Edizione, Sconto) 
+VALUES('69.99', '12.62', '2020-07-17', 'Un open world che miscela stealth e combattimenti epici in un Giappone feudale.', 'Ghost of Tsushima', 'Ghost-Of-Tsushima.avif', 'Definitive Edition', '10.00');
+
+INSERT INTO Gioco (Prezzo, IVA, Data_uscita, Descrizione, Titolo, immagine, Edizione, Sconto) 
+VALUES('79.99', '14.42', '2021-05-07', 'Un survival horror con atmosfere claustrofobiche e un ritmo di gioco incalzante.', 'Resident Evil Village', 'Resident-Evil-Village.jpg', 'Standard', '25.00');
+
+INSERT INTO Gioco (Prezzo, IVA, Data_uscita, Descrizione, Titolo, immagine, Edizione, Sconto) 
+VALUES('99.99', '18.03', '2021-05-07', 'Un survival horror con atmosfere claustrofobiche e un ritmo di gioco incalzante.', 'Resident Evil Village', 'Resident-Evil-Village.jpg', 'Gold Edition', '30.00');
+
