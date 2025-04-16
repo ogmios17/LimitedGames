f6f9af3387094a384dfc15a4e8dbbf54b421e629
oghamSA <o.dellerba@studenti.unisa.it>
2025-04-16 17:14:16 +0200

    add registrazione.java, Registrazione.jsp
    Added files and methods to implement user signing up

---
+++ LimitedGames/src/main/java/Control/Utente/Registrazione.java
@@ -0,0 +1,58 @@
+package Control.Utente;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import Model.Utente.*;
+
+@WebServlet("/Registrazione")
+public class Registrazione extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    public Registrazione() {
+        super();
+    }
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		UtenteBean utente = new UtenteBean();
+		UtenteDAO model = new UtenteDAO();
+		
+		String mail = request.getParameter("email");
+		try {
+			UtenteBean alreadyLogged = model.doRetrieveByKey(mail);
+			if(alreadyLogged.getEmail()!= null) {
+				request.setAttribute("logged",true);
+				RequestDispatcher dispatcher = getServletContext().getRequestDispatcher("/pages/Registrazione.jsp");
+				dispatcher.forward(request, response);
+			}
+			utente.setEmail(mail);
+			utente.setCAP(request.getParameter("cap"));
+			utente.setCitta(request.getParameter("citta"));
+			utente.setCognome(request.getParameter("cognome"));
+			utente.setNome(request.getParameter("email"));
+			utente.setPassword(Hasher.toHash(request.getParameter("password")));
+			utente.setTipo("guest");
+			utente.setUsername(mail);
+			utente.setVia(request.getParameter("via"));
+			
+			model.doSave(utente);
+			RequestDispatcher dispatcher = getServletContext().getRequestDispatcher("/pages/login-form.jsp");
+			dispatcher.forward(request, response);
+		}catch(Exception e) {
+			e.printStackTrace();
+			response.sendRedirect(request.getContextPath()+"/pages/Error.jsp");
+		}
+		
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
---
+++ LimitedGames/src/main/webapp/pages/Registrazione.jsp
@@ -0,0 +1,32 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="ISO-8859-1">
+<title>LimitedGames</title>
+</head>
+<body>
+	
+	<form action="<%= request.getContextPath()%>/Registrazione" method = "POST">
+		<label for="nome">Nome:</label>
+		<input type="text" id="nome" name="nome" required> <br>
+		<label for="cognome">Cognome:</label>
+		<input type="text" id="cognome" name="cognome" required> <br>
+		<label for="email">Email:</label>
+		<input type="text" id="email" name="email" required> <br>
+		<label for="password">Password:</label>
+		<input type="password" id="password" name="password" required> <br>
+		<label for="via">Via:</label>
+		<input type="text" id="via" name="via" required> <br>
+		<label for="cap">CAP:</label>
+		<input type="text" id="cap" name="cap" required> <br>
+		<label for="citta">Città:</label>
+		<input type="text" id="citta" name="citta" required> <br>
+		<input type = "submit" value="Registrati">
+		<% if(Boolean.TRUE.equals(request.getAttribute("logged"))){ %>
+			<p>utente già registrato nella piattaforma</p>
+		<%} %>
+	</form>
+</body>
+</html>
