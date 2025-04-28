ecbc8c83eea0cdae19144e59abd32a04606b853a
oghamSA <o.dellerba@studenti.unisa.it>
2025-04-28 17:50:04 +0200

    change return value of doSave in Gioco
    change return value of doSave in Gioco to save it in stock

--- LimitedGames/src/main/java/Model/Gioco/GiocoDAOInterface.java
+++ LimitedGames/src/main/java/Model/Gioco/GiocoDAOInterface.java
@@ -5,7 +5,7 @@ import java.util.Collection;
 
 public interface GiocoDAOInterface {
 	
-	public void doSave(GiocoBean gioco)throws SQLException;
+	public int doSave(GiocoBean gioco)throws SQLException;
 	public void doDelete(int Id)throws SQLException;
 	public void doUpdate(GiocoBean gioco)throws SQLException;
 	public GiocoBean doRetrieveByKey(int id)throws SQLException;
